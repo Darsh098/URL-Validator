7d59490ab995fcfe90008b0abe031be7d68464c7
Darsh Doshi <darshdoshi01@gmail.com>
2024-09-24 21:33:27 +0530

    Remove unnecessary urlValidator file

--- src/urlValidator.js
+++
@@ -1,9 +0,0 @@
-self.onmessage = function (e) {
-    const url = e.data;
-
-    const pattern = /^(https?|ftp):\/\/(([a-z\d]([a-z\d-]*[a-z\d])?\.)+[a-z]{2,}|localhost)(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i;
-
-    const isValid = !!pattern.test(url);
-
-    self.postMessage(isValid);
-};
